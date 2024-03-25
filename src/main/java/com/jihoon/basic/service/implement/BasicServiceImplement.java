398a137d6aeb86bca50eb10436fb508a4272b7cf
devJiraynor <jiraynor@naver.com>
2024-03-25 14:50:50 +0900

    2024.03.25. 14:50

---
+++ src/main/java/com/jihoon/basic/service/implement/BasicServiceImplement.java
@@ -0,0 +1,12 @@
+package com.jihoon.basic.service.implement;
+
+import com.jihoon.basic.service.BasicService;
+
+public class BasicServiceImplement implements BasicService {
+
+    @Override
+    public String getHello() {
+        return "Hello Springboot!!";
+    }
+    
+}
