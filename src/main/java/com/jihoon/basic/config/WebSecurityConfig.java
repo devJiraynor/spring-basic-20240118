2d6220d79e4dda8d2b221a4286970c0b5f9005ba
devJiraynor <jiraynor@naver.com>
2024-04-16 15:51:19 +0900

    2024.04.16. 15:50

--- src/main/java/com/jihoon/basic/config/WebSecurityConfig.java
+++ src/main/java/com/jihoon/basic/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.jihoon.basic.config;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -17,6 +21,9 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.jihoon.basic.filter.JwtAuthenticationFilter;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -65,7 +72,7 @@ public class WebSecurityConfig {
         .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         // CSRF (Cross-Site Request Forgery): 
         // - 클라이언트(사용자)가 자신의 의도와는 무관한 공격 행위를 하는 것
-        // SQL Injection :
+        // SQL Injection : 
         // - 공격자가 데이터베이스의 쿼리문을 직접 조작하여 데이터를 탈취하는 공격
         // XSS (Cross-Site Scripting) :
         // - 공격자가 웹 브라우저에 악성 스크립트를 작성하여 실행시키는 공격
@@ -83,6 +90,10 @@ public class WebSecurityConfig {
             .requestMatchers("/student", "/student/**").hasRole("STUDENT")
             // 인증된 사용자는 모두 접근을 허용
             .anyRequest().authenticated()
+        )
+        // 인증 과정 중에 발생한 예외 처리
+        .exceptionHandling(exceptionHandling -> exceptionHandling
+            .authenticationEntryPoint(new FailedAuthenticationEntryPoint())
         );
 
         // 우리가 생성한 JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 이전에 등록
@@ -106,4 +117,21 @@ public class WebSecurityConfig {
 
     }
 
+}
+
+// 인증 실패 처리를 위한 커스텀 예외 처리 (AuthenticationEntryPoint 인터페이스 구현)
+class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+        
+        authException.printStackTrace();
+
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.getWriter().write("{ \"message\": \"인증에 실패했습니다.\" }");
+
+    }
+    
 }
